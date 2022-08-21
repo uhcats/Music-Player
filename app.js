ff4bbd994465f4bf2d81fa1110e15b77e6f4ea97
uhcats <stanislawplust@gmail.com>
2022-08-21 15:02:55 +0200

    make progress bar and delete bugs

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-let audio = new Audio('/mp3/music1.mp3');
+let audio = new Audio('');
 
 const text = document.getElementById('text');
 const liText = [...document.querySelectorAll('.li-text')];
@@ -6,6 +6,8 @@ const btns = [...document.querySelectorAll('.choose')];
 const img = document.querySelector('img');
 const play = document.getElementById('play');
 const stop = document.getElementById('stop');
+
+const bar = document.getElementById('bar');
 
 const turnTrackBack = document.querySelector('#turnTrackBack');
 const turnTrackTop = document.querySelector('#turnTrackTop');
@@ -17,6 +19,30 @@ let tabImg = [];
 
 let chooseTrack =  false;
 
+let timer = 0;
+
+
+bar.addEventListener('click', function (e) {
+
+  if(!chooseTrack){
+    alert('You have to choose music');
+    return;
+  }
+  
+
+  let x = e.pageX - this.offsetLeft,
+      clickedValue = (x * this.max / this.offsetWidth).toFixed(0);
+
+      bar.value = clickedValue;
+      timer = clickedValue;
+      audio.currentTime = clickedValue;
+      
+      
+      
+ 
+});
+
+
 
 function TurnTrackInformation(data) {
   let myChoose = data[Number(temp)];
@@ -24,57 +50,87 @@ function TurnTrackInformation(data) {
   img.src = myChoose[1];
   audio = new Audio(myChoose[2]);
   audio.pause();
+  stop.style.display = 'none';
+  play.style.display = 'block'
+  bar.value = 0;
+  timer = 0;
+  chooseTrack = true;
 }
 
 
 function TurnTrack(data) {
+ 
   turnTrackTop.addEventListener('click', () => {
-    
+    audio.pause();
     ++temp;
-    if(temp !== data.length){
+    if(temp !== data.length)
       TurnTrackInformation(data);
-    }else if(temp === data.length){
-      temp = data.length - 1;
-      console.log(temp);
+    else if(temp === data.length){
+      temp = data.length - 1;  
       return;
     }
-
-    
-    
     
   })
 
   turnTrackBack.addEventListener('click', () => {
-   
-   
-    
+    audio.pause();
     if(temp <= 0) return;
     else {
       temp--;
-      TurnTrackInformation(data);
-      
-     
+      TurnTrackInformation(data);     
     }
-    
-
-  })
-  
+  }) 
 }
 
 
-
-
 function playStop() {
+ 
+
+
   play.addEventListener('click', () => {
+    
+
+    if(!chooseTrack){
+      alert('You have to choose music');
+      return;
+    }
+    
     audio.play();
+
+    let time = Number(audio.duration.toFixed(0));
+    bar.max = time;
+
+
+    let timerBar = setInterval(() => {
+      if(stop.style.display === "none")
+        clearInterval(timerBar)
+      if(stop.style.display === "block"){
+        if(timer !== time)
+          bar.value = timer++;
+         else {
+          bar.value = 0;
+          play.style.display = "block";
+          stop.style.display = "none";
+        }
+      }
+    },1000);
+   
+   
+  
+    
+    
     play.style.display = "none"
     stop.style.display = "block"
   })
   stop.addEventListener('click', () => {
     audio.pause();
+    
     stop.style.display = "none"
     play.style.display = "block"
   })
+
+
+
 };
 
 function showInforamtion(data) {
@@ -87,9 +143,12 @@ function showInforamtion(data) {
  btns.forEach((btn,index) => {
   btn.setAttribute('id', index);
   btn.addEventListener('click', (e) => {
+    chooseTrack = true;
     temp = e.target.id;
     stop.style.display = 'none';
-    play.style.display = 'block'
+    play.style.display = 'block';
+    bar.value = 0;
+    timer = 0;
     audio.pause();
     let myChoose = data[e.target.id];
 
@@ -102,6 +161,7 @@ function showInforamtion(data) {
     
 
   })
+ 
  });
 
  
@@ -120,13 +180,7 @@ function showText(){
   .then(data => showInforamtion(data))
 }
 
-
-
-
-
-showText();
-// showPicture();
 playStop();
 
-  
+showText();
 
